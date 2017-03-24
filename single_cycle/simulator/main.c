11171538c50f81b4e1c724fa08fdd6a3deaa194b
s103030015 <a103291@yahoo.com.tw>
2017-03-24 11:17:35 +0800

    Update main.c

--- single_cycle/simulator/main.c
+++ single_cycle/simulator/main.c
@@ -17,3 +17,25 @@ static int TypeToReg(RegType reg, Instruction *instr)
   switch(reg){
     case RS:
       return instr->rs;
+    case RT:
+      return instr->rt;
+    case RD:
+      return instr->rd;
+    case EXTRA:
+      return instr->extra;
+    default:
+      return -1;
+   }
+}
+
+void Instruction(Instructioin *instr)
+{
+  int byte, raw, nextLoadReg, nextLoadValue;
+  nextLoadReg=0;
+  nextLoadValue=0;
+  
+  if(ReadMem(registers[PCReg], 4, &raw)==0) return;
+  instr->value = raw;
+  instr->Decode();
+  
+  struct OpString 
