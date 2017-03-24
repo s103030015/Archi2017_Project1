4b5927dd48c7748407eee41209c54994d283bf82
s103030015 <a103291@yahoo.com.tw>
2017-03-24 12:15:50 +0800

    Update main.c

--- single_cycle/simulator/main.c
+++ single_cycle/simulator/main.c
@@ -34,8 +34,86 @@ void Instruction(Instructioin *instr)
   nextLoadReg=0;
   nextLoadValue=0;
   
-  if(ReadMem(registers[PCReg], 4, &raw)==0) return;
-  instr->value = raw;
-  instr->Decode();
+  int pcAfter=register[NextPCReg]+4;
+  int sum, diff, tmp, value;
+  unsigned int rs, rt, imm;
   
-  struct OpString 
+  switch(instr->opCode)
+  {
+    case ADD:
+      sum=register[instr->rs]+instr->extra;
+      register[instr->rt]=sum;
+      break;
+    case ADDU:
+      register[instr->rd]=register[instr->rs]+register[instr->rt];
+      break;
+    case SUB:
+      diff=register[instr->rs]-register[instr->rt];
+      register[instr->rd]=diff;
+      break;
+    case AND:
+      register[instr->rd]=register[instr->rs]+register[instr->rt];
+      break;
+    case OR:
+      register[instr->rd]=register[instr->rs] | register[instr->rt];
+      break;
+    case XOR:
+      register[instr->rd]=register[instr->rs] ^ register[instr->rt];
+    case NOR:
+      register[instr->rd]=~(register[instr->rs] | register[instr->rt]);
+      break;
+    case NAND:
+      register[instr->rd]=~(register[instr->rs]+register[instr->rt]);
+      break;
+    case SLT:
+      if(register[instr->rs] < register[instr->rt]) register[instr->rd]=1;
+      else register[instr->rd]=0;
+      break;
+    case SLL:
+      register[instr->rd]=register[instr->rt] << instr->extra;
+      break;
+    case SRL:
+      tmp=register[instr->rt];
+      tmp >>= instr->extra;
+      register[instr->rd]=tmp;
+      break;
+    case SRA:
+      register[instr->rd]=register[instr->rt] >> instr->extra;
+      break;
+    case JR:
+      pcAfter=register[instr->rs];
+      break;
+    case MULT:
+      Mult(register[instr->rs], register[instr->rt], TRUE, &register[HiReg], &register[LoReg]);
+      break;
+    case MULTU:
+      Mult(register[instr->rs], register[instr->rt], FALSE, &register[HiReg], &register[LoReg]);
+      break;
+    case MFHI:
+      register[instr->rd]=register[HiReg];
+      break;
+    case MFLO:
+      register[instr->rd]=register[LoReg];
+      break;
+    case ADDI:
+      sum=register[instr->rs]+instr->extra;
+      register[instr->rt]=sum;
+      break;
+    case ADDIU:
+      register[instr->rt]=register[instr->rs]+instr->extra;
+      break;
+    case LW:
+      tmp=register[instr->rs]+instr->extra;
+      if(tmp & 0x3)
+      {
+        RaiseException (AddressErrorException, tmp);
+        return;
+      }
+      if(!ReadMem(tmp, 4, &value)) return;
+      nextLoadReg = instr->rt;
+      nextLoadValue = value;
+      break;
+      
+      
+      
+      
