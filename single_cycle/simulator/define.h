5d609af5fc4868fa3ea5c86c7eab08fd51458053
s103030015 <a103291@yahoo.com.tw>
2017-03-25 20:59:02 +0800

    Update define.h

--- single_cycle/simulator/define.h
+++ single_cycle/simulator/define.h
@@ -45,78 +45,8 @@
 #define WordToAdd(x) ((x)<<2)
 #define AddToWord(x) ((x)>>2)
 
+#define SIGN_BIT	0x80000000
+
 #define RFORM 1
 #define IFORM 2
 #define JFORM 3
-
-struct op_info{
-  int opcode;
-  int format;
-};
-
-static op_info op_table[] = {
-  {ADD, RFORM}, {ADDU, RFORM}, {SUB, RFORM}, {AND, RFORM},
-  {OR, RFORM}, {XOR, RFORM}, {NOR, RFORM}, {NAND, RFORM},
-  {SLT, RFORM}, {SLL, RFORM}, {SRL, RFORM}, {SRA, RFORM},
-  {JR, RFORM}, {MULT, RFORM}, {MULTU, RFORM}, {MFHI, RFORM},
-  {MFLO, RFORM}, {ADDI, IFORM},{ADDIU, IFORM}, {LW, IFORM},
-  {LH, IFORM}, {LHU, IFORM}, {LB, IFORM}, {LBU, IFORM},
-  {SW, IFORM}, {SH, IFORM}, {SB, IFORM}, {LUI, IFORM},
-  {ANDI, IFORM}, {ORI, IFORM}, {NORI, IFORM}, {SLTI, IFORM},
-  {BEQ, IFORM}, {BNE, IFORM}, {BGTZ, IFORM}, {J, JFORM},
-  {JAL, JFORM}
-};
-
-enum RegType { NONE, RS, RT, RD, EXTRA};
-
-struct op_string{
-  char *format;
-  RegType arg[3];
-};
-
-static struct op_string  opString[] ={
-  {"ADD r%d,r%d,r%d", {RD, RS, RT}},
-  {"ADDU r%d,r%d,r%d", {RD, RS, RT}},
-  {"SUB r%d,r%d,r%d", {RD, RS, RT}},
-  {"AND r%d,r%d,r%d", {RD, RS, RT}},
-  {"OR r%d,r%d,r%d", {RD, RS, RT}},
-  {"XOR r%d,r%d,r%d", {RD, RS, RT}},
-  {"NOR r%d,r%d,r%d", {RD, RS, RT}},
-  {"NAND r%d,r%d,r%d", {RD, RS, RT}},
-  {"SLT r%d,r%d,r%d", {RD, RS, RT}},
-  {"SLL r%d r%d %d", {RD, RT, EXTRA}},
-  {"SRL r%d r%d %d", {RD, RT, EXTRA}},
-  {"SRA r%d,r%d,%d", {RD, RT, EXTRA}},
-  {"JR r%d r%d", {RD, RS, NONE}},
-  {"MULT r%d,r%d", {RS, RT, NONE}},
-  {"MULTU r%d,r%d", {RS, RT, NONE}},
-  {"MFHI r%d", {RD, NONE, NONE}},
-  {"MFLO r%d", {RD, NONE, NONE}},
-  {"ADDI r%d,r%d,%d", {RT, RS, EXTRA}},
-  {"ADDIU r%d,r%d,%d", {RT, RS, EXTRA}},
-  {"LW r%d,%d(r%d)", {RT, EXTRA, RS}},
-  {"LH r%d,%d(r%d)", {RT, EXTRA, RS}},
-  {"LHU r%d,%d(r%d)", {RT, EXTRA, RS}},
-  {"LB r%d,%d(r%d)", {RT, EXTRA, RS}},
-  {"LBU r%d,%d(r%d)", {RT, EXTRA, RS}},
-  {"SW r%d,%d(r%d)", {RT, EXTRA, RS}},
-  {"SH r%d,%d(r%d)", {RT, EXTRA, RS}},
-  {"SB r%d,%d(r%d)", {RT, EXTRA, RS}},
-  {"LUI r%d,%d", {RT, EXTRA, NONE}},
-  {"ANDI r%d,r%d,%d", {RT, RS, EXTRA}},
-  {"ORI r%d,r%d,%d", {RT, RS, EXTRA}},
-  {"NORI r%d,r%d,%d", {RT, RS, EXTRA}},
-  {"SLTI r%d,r%d,%d", {RT, RS, EXTRA}},
-  {"BEQ r%d,r%d,%d", {RS, RT, EXTRA}},
-  {"BNE r%d,r%d,%d", {RS, RT, EXTRA}},
-  {"BGTZ r%d,%d", {RS, EXTRA, NONE}},
-  {"J %d", {EXTRA, NONE, NONE}},
-  {"JAL %d", {EXTRA, NONE, NONE}}
-};
-  
-typedef struct instruction{
-  char opCode;
-  unsigned int value;
-  char rs, rt, rd;
-  int extra;
-}INST;
